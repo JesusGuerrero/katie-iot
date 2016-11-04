978667d24a1e794876281672397cdd1ce0c5ebd4
katkat100 <k.noborikawa.chs@gmail.com>
2016-11-04 14:50:03 -0700

    Add files via upload
    from laptop

---
+++ test/select.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-1.11.3.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
