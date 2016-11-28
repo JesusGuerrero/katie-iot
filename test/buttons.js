96f63e1824bb96a07531aa73e0ed3d7b61efa06b
katkat100 <k.noborikawa.chs@gmail.com>
2016-11-28 15:13:12 -0800

    Add files via upload

---
+++ test/buttons.js
@@ -0,0 +1,41 @@
+console.log("button test is running")
+
+var Gpio = require('onoff').Gpio,	//onoff module (use npm install onoff)
+  buttonA = new Gpio(4, 'in', 'both');	//setup GPIO26 as output
+  buttonB = new Gpio(22, 'in', 'both');	//setup GPIO26 as output
+  buttonC = new Gpio(26, 'in', 'both');	//setup GPIO26 as output
+
+buttonA.setActiveLow( true );
+buttonB.setActiveLow( true );
+buttonC.setActiveLow( true );
+
+buttonA.watch(function(err,state){
+	if(state == 1){
+		console.log("buttonA on");
+	} else {
+		console.log("buttonA off");
+	}
+});
+
+buttonB.watch(function(err,state){
+	if(state == 1){
+		console.log("buttonB on");
+	} else {
+		console.log("buttonB off");
+	}
+});
+
+buttonC.watch(function(err,state){
+	if(state == 1){
+		console.log("buttonC on");
+	} else {
+		console.log("buttonC off");
+	}
+});
+
+process.on('SIGINT', function(){
+  buttonA.unexport();
+  buttonB.unexport();
+  buttonC.unexport();
+  process.exit();
+});
