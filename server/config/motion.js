608ed10491689e6884fd288c3d651e79c3b48494
katkat100 <k.noborikawa.chs@gmail.com>
2016-12-04 13:29:11 -1000

    Add files via upload

--- server/config/motion.js
+++ server/config/motion.js
@@ -1,16 +1,3 @@
-var gpio = require('onoff').Gpio,
-  motion = new gpio(21, 'in', 'both'),
-  io = null;
-
-motion.watch( function(err, val) {
-  if( err ) { console.log('Motion in 22 Error'); return; }
-
-  console.log('Motion in 22 is ' +(val ? 'ACTIVE' : 'INACTIVE') + ' : ' + new Date().toLocaleString() );
-  if( io ) {
-    io.sockets.emit('event:button', val);
-  }
-
-});
 var Gpio = require('onoff').Gpio,	//onoff module (use npm install onoff)
   buttonA = new Gpio(4, 'in', 'both');	//setup GPIO26 as output
   buttonB = new Gpio(22, 'in', 'both');	//setup GPIO26 as output
